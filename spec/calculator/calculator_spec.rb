5568e07839de7ec2bbe729aacb4beeb565785263
Igor Baesse de Araujo <igorbaesse@timcoo.com.br>
2018-01-31 22:26:25 -0200

    Class 1 context and better test

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,14 +1,21 @@
 require 'Calculator'
 describe Calculator do
-  it 'sum method for 2 numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, 7)
-    expect(result).to eq(12)
-  end
+  context '#sum' do
+    it 'with positive numbers' do
+      calc = Calculator.new
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)
+    end
 
-  it 'sum method for 2 numbers with negative number and float numbers' do
-    calc = Calculator.new
-    result = calc.sum(-5, 7.8)
-    expect(result).to eq(2.8)
+    it 'negative numbers and float numbers' do
+      calc = Calculator.new
+      result = calc.sum(-5, 7.8)
+      expect(result).to eq(2.8)
+    end
+    it 'negative numbers ' do
+      calc = Calculator.new
+      result = calc.sum(-5, -7.8)
+      expect(result).to eq(-12.8)
+    end
   end
 end
