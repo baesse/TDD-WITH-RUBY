5cc06850ae4b54d80516a98a889b4e4bdf7d70b6
Igor Baesse de Araujo <igorbaesse@timcoo.com.br>
2018-01-31 22:34:53 -0200

    Comand xit to ignore test

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -2,19 +2,22 @@ require 'Calculator'
 describe Calculator do
   context '#sum' do
     it 'with positive numbers' do
-      calc = Calculator.new
-      result = calc.sum(5, 7)
+      result = subject.sum(5, 7)
       expect(result).to eq(12)
     end
 
     it 'negative numbers and float numbers' do
-      calc = Calculator.new
-      result = calc.sum(-5, 7.8)
+      result = subject.sum(-5, 7.8)
       expect(result).to eq(2.8)
     end
     it 'negative numbers ' do
-      calc = Calculator.new
-      result = calc.sum(-5, -7.8)
+      result = subject.sum(-5, -7.8)
+      expect(result).to eq(-12.8)
+    end
+
+    #ignore teste xit
+    xit 'negative numbers ' do
+      result = subject.sum(-5, -7.8)
       expect(result).to eq(-12.8)
     end
   end
